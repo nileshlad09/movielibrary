75531a8f0ab221eec88bf19da8a5a725930dde03
Nilesh <nileshlad871@gmail.com>
2022-07-31 12:22:17 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,18 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
+const PORT = process.env.PORT || 5000
+
 app.use('/api/watchlist',require('./routes/watchlists'))
 app.use('/api/watched',require('./routes/watched'))
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/review',require('./routes/review'))
 
-app.listen(5000,()=>{
-    console.log("Port:5000");
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static("Frontend/build"));
+}
+
+
+app.listen(PORT,()=>{
+    console.log(PORT);
 })
