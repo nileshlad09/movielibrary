9a3c85ecd3639bb699df7d111a22b490b964bcbe
Nilesh <nileshlad871@gmail.com>
2022-08-07 12:50:43 +0530

    make it better

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,25 @@
 const express = require('express')
 const cors = require("cors")
 const connectToMongo = require('./db')
+require('dotenv').config()
 connectToMongo()
 const app = express();
 app.use(cors())
 app.use(express.json())
-const NODE_ENV = "production"
-const PORT = 5000
+
+const PORT = process.env.PORT
 app.use('/api/watchlist',require('./routes/watchlists'))
 app.use('/api/watched',require('./routes/watched'))
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/review',require('./routes/review'))
 
-if(NODE_ENV==="production"){
-    app.use(express.static(path.join("frontend/build")));
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
-    })
-}
 
+if(process.env.NODE_ENV==="production"){
+      app.use(express.static(path.join("../build")))  
+      app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"build","index.html"))
+      })
+}
 
 app.listen(PORT,()=>{
     console.log("Server running on:",PORT);
