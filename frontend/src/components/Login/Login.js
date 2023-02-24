d591555b71fef2167d0cdcab186fa796e6c950ea
nilesh <nileshlad871@gmail.com>
2023-02-24 17:38:24 +0530

    login signup improved

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import './login.css'
-import 'react-slideshow-image/dist/styles.css'
 import Signup from "./Signup";
 import LoginMain from "./LoginMain";
 
